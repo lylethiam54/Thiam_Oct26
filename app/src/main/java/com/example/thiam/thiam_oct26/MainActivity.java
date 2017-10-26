790bd6bf3b267935a65742a3e54c2ae228c4ea2e
lylethiam54 <lylethiam@gmail.com>
2017-10-26 22:59:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/thiam/thiam_oct26/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.thiam.thiam_oct26;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btnSave, btnLoad;
+    EditText etMessage;
+    TextView tvDisplay;
+    FileOutputStream fos;
+    FileInputStream fis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnSave = (Button) findViewById(R.id.btn_save);
+        btnLoad = (Button) findViewById(R.id.btn_display);
+        etMessage = (EditText) findViewById(R.id.et_message);
+        tvDisplay = (TextView) findViewById(R.id.tv_display);
+
+    }
+
+    public void saveMessage(View view) {
+
+        String message = etMessage.getText().toString();
+        try {
+            fos = openFileOutput("output.txt", Context.MODE_PRIVATE);
+            fos.write(message.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Toast.makeText(this, "Message saved", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+
+    public void displayMessage(View view) {
+        StringBuffer buffer = new StringBuffer();
+        int read = 0;
+        try {
+            fis = openFileInput("output.txt");
+            while ((read = fis.read()) != -1) {
+                buffer.append((char) read);
+            }
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tvDisplay.setText(buffer.toString());
+    }
+}
